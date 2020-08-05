28e1c907ce6c396eae57961b33bf261e693ce9c3
ele-gall-ac-mineducation <54439757+ele-gall-ac-mineducation@users.noreply.github.com>
2020-08-05 19:15:40 +0200

    amélioration + ajouté un exemple
    amélioration : recherche exacte de la mention cf https://www.numerique.gouv.fr/publications/rgaa-accessibilite/obligations/#mention-obligatoire-sur-la-page-daccueil
    après quasi-normalisation des espaces (répétitions, sauts de ligne…)
    ajouté un exemple de fichier d’entrée et son résultat résultat, avec des cas divers

--- mention_RGAA_URLs.py
+++ mention_RGAA_URLs.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 # licence ouverte https://www.etalab.gouv.fr/licence-ouverte-open-licence - travail personnel sur temps libre
-# « vérifie » à peu près la présence des mentions obligatoires RGAA 4 de la page d’accueil d’un « service de communication au public en ligne » :
-# « Accessibilité : non conforme »
-# « Accessibilité : partiellement conforme »
-# « Accessibilité : totalement conforme »
+# vérifie la présence des mentions obligatoires RGAA 4 de la page d’accueil d’un « service de communication au public en ligne » :
+# « Accessibilité : non conforme »
+# « Accessibilité : partiellement conforme »
+# « Accessibilité : totalement conforme »
+# aux espaces près (doublées*, insécables, retours à la ligne…)
+# * https://www.w3.org/TR/1999/REC-xpath-19991116/#function-normalize-space
 # en cas de réussite prend une capture d’écran pour vérification postérieure
 # prend en entrée une liste d’URL (sans http… devant), en éliminant les lignes débutant par un #
 # pas parfait mais mieux que rien… (notamment des erreurs qui bouclent parfois) :
-# merci à Bastien et Nicolas (de me persuader de publier un code dans un tel état… ;)
+# merci à Bastien et Nicolas (de me persuader de publier un code dans un tel état… même s’il se bonifie ;)
 # contributions de pythonistes bienvenues (n’en suis pas)
+# v1.0 : prise en compte exacte de la mention.
+# TODO Cas des mentions multiples incohérentes non traité cf www.numerique.gouv.fr/publications/rgaa-accessibilite/obligations ;)
+# TODO Cas des URLs fausses à catcher (non loguées actuellement)
+# v0.9 : version initiale
 from selenium import webdriver
 import time
 
@@ -28,31 +34,38 @@ logfile = open('log.txt', 'w')
 logfile.write( 'START' + "\n" )
 
 for url in URLs:
+	logfile.write( url + "\n" )
 	try:
-		browser.get('https://' + url +"\n")
-		logfile.write( url + "\n" )
+		browser.get('http://' + url +"\n")
 		time.sleep(1)
 		logfile.write( browser.current_url + "\n" )
 
 		body = browser.find_element_by_xpath('//body')
 
+		# note : translate(./text(),' ',' ') est en fait translate(./text(),'&nbsp;',' ') = remplacement des espaces insécables (indistinguables)
 		mentions_100 = browser.find_elements_by_xpath(
-			"//*[contains(.,'Accessibilité')][contains(.,'totalement conforme')]" # tous les éléments contenant la mention légalement imposée
-			+"[not(.//*[contains(.,'Accessibilité')][contains(.,'totalement conforme')])]" # sans enfant la contenant
+			#  tous les éléments (×3)
+			"//*"
+			# contenant la mention légalement imposée
+			+"[         contains(normalize-space(translate(translate(translate(./text(),' ',' '),'\r',' '),'\n',' ')),'Accessibilité : totalement conforme')]"
+			# sans enfant la contenant
+			+"[not(.//*[contains(normalize-space(translate(translate(translate(./text(),' ',' '),'\r',' '),'\n',' ')),'Accessibilité : totalement conforme')])]"
 		) # -> les nœuds les plus profonds contenant la mention, et pas leur ancêtres
 		mentions_50 = browser.find_elements_by_xpath(
-			"//*[contains(.,'Accessibilité')][contains(.,'partiellement conforme')]" # tous les éléments contenant la mention légalement imposée
-			+"[not(.//*[contains(.,'Accessibilité')][contains(.,'partiellement conforme')])]" # sans enfant la contenant
-		) # -> les nœuds les plus profonds contenant la mention, et pas leur ancêtres
+			"//*"
+			+"[         contains(normalize-space(translate(translate(translate(./text(),' ',' '),'\r',' '),'\n',' ')),'Accessibilité : partiellement conforme')]"
+			+"[not(.//*[contains(normalize-space(translate(translate(translate(./text(),' ',' '),'\r',' '),'\n',' ')),'Accessibilité : partiellement conforme')])]"
+		)
 		mentions_00 = browser.find_elements_by_xpath(
-			"//*[contains(.,'Accessibilité')][contains(.,'non conforme')]" # tous les éléments contenant la mention légalement imposée
-			+"[not(.//*[contains(.,'Accessibilité')][contains(.,'non conforme')])]" # sans enfant la contenant
-		) # -> les nœuds les plus profonds contenant la mention, et pas leur ancêtres
+			"//*"
+			+"[         contains(normalize-space(translate(translate(translate(./text(),' ',' '),'\r',' '),'\n',' ')),'Accessibilité : non conforme')]"
+			+"[not(.//*[contains(normalize-space(translate(translate(translate(./text(),' ',' '),'\r',' '),'\n',' ')),'Accessibilité : non conforme')])]"
+		)
 
-		mentions = mentions_00 + mentions_50+ mentions_100
+		mentions = mentions_00 + mentions_50 + mentions_100
 
 		# ~ logfile.write(mentions + "\n" )
-		logfile.write(str(len(mentions)) + "\n" )
+		# ~ logfile.write(str(len(mentions)) + "\n" )
 
 		if (0 < len(mentions)) :
 			time.sleep(2)
@@ -65,7 +78,7 @@ for url in URLs:
 				browser.execute_script("arguments[0].setAttribute('style','outline: 5px ridge red ; outline-offset: 3px ; z-index: 999999 ; ');", mention);
 			time.sleep(2)
 			fichier = url.replace('/','·') + '_screenshot.png'
-			browser.get_screenshot_as_file(fichier)
+			# ~ browser.get_screenshot_as_file(fichier)
 			logfile.write(fichier + "\n" )
 			if (0 < len(mentions_00)) :
 				logfile.write('mention : non conforme' + "\n" )
@@ -81,7 +94,7 @@ for url in URLs:
 	finally:
 		logfile.write( "\n" );
 
-browser.quit()
+# ~ browser.quit()
 
 logfile.write( 'FINISH' + "\n" )
 
